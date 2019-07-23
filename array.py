6edf08682f75de20e05d0089c7f8fe0bf9618742
harry-martins <52189030+harry-martins@users.noreply.github.com>
2019-07-23 21:52:39 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,10 +1,10 @@
 N,K=map(int,input().split())
-arr=[]
-arr=[int(i) for i in input().split()]
+a=[]
+a=[int(i) for i in input().split()]
 r=0
 n=0
 while K>0:
-	n+=arr[r]
+	n+=a[r]
 	K=K-1
 	r=r+1
 print(n)
